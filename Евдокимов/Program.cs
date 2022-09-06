ef91f16aa82f67abba3c4959a28f8bc2bae54401
Al14n <112982613+Al14n@users.noreply.github.com>
2022-09-07 00:24:56 +0300

    Add files via upload

---
+++ Евдокимов/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Евдокимов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int a = 0;
+            int[] L = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] Y = { 5, 4, 3, 2, 1 };
+            for(int i = 0; i < L.Length; i++)
+            {
+                for (int j = 0; j < Y.Length; j++)
+                {
+                    result = result + Y[j] ^ 3;
+                }
+                a = a + ((L[i] ^ 2 / (L[i] + 1)) - (L[0] * L[6]) * result);
+
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Евдокимов2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Евдокимов2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] familii = new string[] { "Иванов", "Есаул", "Викторов", "Бобров" };
+            bool check = false;
+
+            for (int i = 0; i < familii.Length; i++)
+            {
+                if (familii[i] == "Иванов")
+                {
+
+                    check = true;
+                    Console.WriteLine("Фамилия Иванов есть");
+                }
+            }
+            if (!check)
+                Console.WriteLine("Иванов отсутствует");
+        }
+    }
+}
